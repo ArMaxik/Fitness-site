4fbdaed7975d1bf1378a8ef6ca553347c0e6f193
Slava Eliseev <eliseev.1999.slava@mail.ru>
2018-12-10 16:14:29 +0300

    remove exercises model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'exercises/create'
-  get 'exercises/delete'
-  get 'exercises/show'
   get 'trainer/feed'
   root 'trainer#feed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_09_113313) do
-
-  create_table "exercises", force: :cascade do |t|
-    t.text "title"
-    t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
