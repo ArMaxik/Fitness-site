70ba616e4dfe13d837d2218839963dc8519de808
Slava Eliseev <eliseev.1999.slava@mail.ru>
2018-12-22 20:20:25 +0300

    users attaches to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_22_103922) do
+ActiveRecord::Schema.define(version: 2018_12_22_150644) do
 
   create_table "comments", force: :cascade do |t|
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "exercise_id"
+    t.integer "user_id"
   end
 
   create_table "exercises", force: :cascade do |t|
