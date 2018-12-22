e41e62c82056b3ca2935aaeb2e43c09cc5250808
Slava Eliseev <eliseev.1999.slava@mail.ru>
2018-12-23 02:07:37 +0300

    repair bug with new posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
     resources :comments
     post 'change_rating'
   end
+  get 'users/:id', to: 'users#show', as: 'user'
   get 'feed', to: 'exercises#feed'
   root 'exercises#feed'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
